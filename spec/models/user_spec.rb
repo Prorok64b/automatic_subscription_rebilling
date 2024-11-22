9a31850632f08fd2dae3b777fb8234dfaeb530d2
Vadym Filipov <vadym.filipov@outlook.com>
2024-11-22 22:53:10 +0100

    User#primary_bank_card add spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { build(:user) }
+
+  describe '#primary_bank_card' do
+    subject { user.primary_bank_card }
+
+    before do
+      create(:bank_card, :primary, id: 1, user: user)
+      create(:bank_card, id: 2, user: user)
+      create(:bank_card, id: 3, user: user)
+    end
+
+    it 'returns primary bank card' do
+      expect(subject.id).to eq(1)
+    end
+  end
 end
