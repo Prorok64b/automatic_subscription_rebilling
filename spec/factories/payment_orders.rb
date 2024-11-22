e2a56a2a8a1cbd026c2fceac984d8e10afe298e3
Vadym Filipov <vadym.filipov@outlook.com>
2024-11-22 23:45:06 +0100

    extend factories

--- spec/factories/payment_orders.rb
+++ spec/factories/payment_orders.rb
@@ -4,12 +4,24 @@ FactoryBot.define do
     cash_amount { BigDecimal('49.99') }
     percentage_paid { 50 }
 
+    trait :created do
+      status { PaymentOrder::Statuses::CREATED }
+    end
+
+    trait :pending do
+      status { PaymentOrder::Statuses::PENDING }
+    end
+
     trait :success do
-      status { 'success' }
+      status { PaymentOrder::Statuses::SUCCESS }
+    end
+
+    trait :partial_success do
+      status { PaymentOrder::Statuses::PARTIAL_SUCCESS }
     end
 
     trait :fail do
-      status { 'fail' }
+      status { PaymentOrder::Statuses::FAIL }
     end
   end
 end
--- spec/factories/subscriptions.rb
+++ spec/factories/subscriptions.rb
@@ -1,7 +1,16 @@
 FactoryBot.define do
   factory :subscription do
     percentage_paid { 100 }
-    next_payment_on { Date.today + 1.month }
+    payment_on { Date.today + 1.month }
+    price { BigDecimal('34.99') }
+
+    trait :inactive do
+      active { false }
+    end
+
+    trait :active do
+      active { true }
+    end
 
     user
   end
