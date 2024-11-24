60612b5129c940432a55054146e20568ce01ca23
Vadym Filipov <vadym.filipov@outlook.com>
2024-11-24 15:45:45 +0100

    BankCard model, add validation, allow only one primary card per user

--- app/models/bank_card.rb
+++ app/models/bank_card.rb
@@ -7,5 +7,15 @@ class BankCard < ApplicationRecord
   validates :expiry_month, presence: true
   validates :expiry_year, presence: true
   validates :cvv, presence: true
-  validates :primary, presence: true, uniqueness: { scope: :user_id }
+
+  validate :only_one_primary_per_user
+
+  private
+
+  def only_one_primary_per_user
+    return unless primary?
+    return if self.class.primary.where(user_id: user.id).count.zero?
+
+    errors.add(:base, 'Only one primary card per user allowed')
+  end
 end
--- spec/models/bank_card_spec.rb
+++ spec/models/bank_card_spec.rb
@@ -16,4 +16,29 @@ describe BankCard, type: :model do
       it { is_expected.to eq([primary_card.id]) }
     end
   end
+
+  describe 'only one primary card per user allowed' do
+    subject { new_instance.save }
+
+    let!(:user) { create(:user) }
+    let(:new_instance) { build(:bank_card, :primary, user: user) }
+
+    it { is_expected.to eq(true) }
+
+    context 'when user has a primary card' do
+      before do
+        create(:bank_card, :primary, user: user)
+      end
+
+      it { is_expected.to eq(false) }
+
+      it 'returns proper error' do
+        subject
+
+        errors = new_instance.errors.messages
+
+        expect(errors).to eq({ base: ['Only one primary card per user allowed'] })
+      end
+    end
+  end
 end
