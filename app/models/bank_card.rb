a8a7d091b20800ee4c16d2208712375a8ad0b7b4
Vadym Filipov <vadym.filipov@outlook.com>
2024-11-24 15:16:51 +0100

    BankCard model, add .primary scope

--- app/models/bank_card.rb
+++ app/models/bank_card.rb
@@ -1,6 +1,8 @@
 class BankCard < ApplicationRecord
   belongs_to :user
 
+  scope :primary, -> { where(primary: true) }
+
   validates :number, presence: true
   validates :expiry_month, presence: true
   validates :expiry_year, presence: true
--- spec/models/bank_card_spec.rb
+++ spec/models/bank_card_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe BankCard, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe BankCard, type: :model do
+  describe 'Scopes' do
+    describe '.primary' do
+      subject { described_class.primary.pluck(:id) }
+
+      let!(:user) { create(:user) }
+      let!(:primary_card) { create(:bank_card, :primary, user: user) }
+
+      before do
+        create(:bank_card, user: user)
+        create(:bank_card, user: user)
+      end
+
+      it { is_expected.to eq([primary_card.id]) }
+    end
+  end
 end
