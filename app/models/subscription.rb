b90cc603974dd9df0f73c57b23db465ec3324d12
Vadym Filipov <vadym.filipov@outlook.com>
2024-11-26 12:39:29 +0100

    Subscription model, add helper methods: fully_paid?, partialy_paid?, unpaid?

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,7 +2,17 @@ class Subscription < ApplicationRecord
   belongs_to :user
 
   validates :percentage_paid, presence: true, inclusion: { in: [0, 25, 50, 75, 100] }
-  validates :next_payment_on, presence: true
+  validates :payment_on, presence: true
 
-  # TODO: add 'cash_amount' field
+  def fully_paid?
+    cash_amount_paid == price
+  end
+
+  def partialy_paid?
+    cash_amount_paid > 0 && cash_amount_paid < price
+  end
+
+  def unpaid?
+    cash_amount_paid.zero?
+  end
 end
--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -1,5 +1,69 @@
 require 'rails_helper'
 
-RSpec.describe Subscription, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Subscription, type: :model do
+  describe "#fully_paid?" do
+    subject { instance.fully_paid? }
+
+    context 'when given fully paid subscription' do
+      let(:instance) { build(:subscription, cash_amount_paid: 100, price: 100) }
+
+      it { is_expected.to eq(true) }
+    end
+
+    context 'when given partialy paid subscription' do
+      let(:instance) { build(:subscription, cash_amount_paid: 50, price: 100) }
+
+      it { is_expected.to eq(false) }
+    end
+
+    context 'when given unpaid subscription' do
+      let(:instance) { build(:subscription, cash_amount_paid: 0, price: 100) }
+
+      it { is_expected.to eq(false) }
+    end
+  end
+
+  describe "#partialy_paid?" do
+    subject { instance.partialy_paid? }
+
+    context 'when given fully paid subscription' do
+      let(:instance) { build(:subscription, cash_amount_paid: 100, price: 100) }
+
+      it { is_expected.to eq(false) }
+    end
+
+    context 'when given partialy paid subscription' do
+      let(:instance) { build(:subscription, cash_amount_paid: 50, price: 100) }
+
+      it { is_expected.to eq(true) }
+    end
+
+    context 'when given unpaid subscription' do
+      let(:instance) { build(:subscription, cash_amount_paid: 0, price: 100) }
+
+      it { is_expected.to eq(false) }
+    end
+  end
+
+  describe "#unpaid?" do
+    subject { instance.unpaid? }
+
+    context 'when given fully paid subscription' do
+      let(:instance) { build(:subscription, cash_amount_paid: 100, price: 100) }
+
+      it { is_expected.to eq(false) }
+    end
+
+    context 'when given partialy paid subscription' do
+      let(:instance) { build(:subscription, cash_amount_paid: 50, price: 100) }
+
+      it { is_expected.to eq(false) }
+    end
+
+    context 'when given unpaid subscription' do
+      let(:instance) { build(:subscription, cash_amount_paid: 0, price: 100) }
+
+      it { is_expected.to eq(true) }
+    end
+  end
 end
