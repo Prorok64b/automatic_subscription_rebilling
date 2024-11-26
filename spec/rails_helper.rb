196d692f20352b7079353a2a0f72425baf594cff
Vadym Filipov <vadym.filipov@outlook.com>
2024-11-26 12:33:50 +0100

    fix rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -14,6 +14,7 @@ require 'rspec/rails'
 
 require 'support/database_cleaner'
 require 'support/factory_bot'
+require 'support/sidekiq'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
