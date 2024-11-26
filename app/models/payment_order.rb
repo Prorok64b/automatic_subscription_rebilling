7877c2f900a0082946ce0801447ba19f06245e19
Vadym Filipov <vadym.filipov@outlook.com>
2024-11-26 12:35:18 +0100

    PaymentOrder model add contants for statuses

--- app/models/payment_order.rb
+++ app/models/payment_order.rb
@@ -1,8 +1,26 @@
 class PaymentOrder < ApplicationRecord
+  class Statuses
+    CREATED            = 'created'.freeze
+    PENDING            = 'pending'.freeze
+    SUCCESS            = 'success'.freeze
+    INSUFFICIENT_FUNDS = 'insufficient_funds'.freeze
+    PARTIAL_SUCCESS    = 'partial_success'.freeze
+    ERROR              = 'error'.freeze
+  end
+
   belongs_to :subscription
 
-  enum status: { pending: 'pending', success: 'success', fail: 'fail' }
+  enum status: {
+    created:           Statuses::CREATED,
+    pending:           Statuses::PENDING,
+    success:           Statuses::SUCCESS,
+    insufficient_funds: Statuses::INSUFFICIENT_FUNDS,
+    partial_success:   Statuses::PARTIAL_SUCCESS,
+    error:             Statuses::ERROR
+  }
 
   validates :cash_amount, presence: true
+
+  # TODO: remove percentage_paid
   validates :percentage_paid, presence: true, inclusion: { in: [0, 25, 50, 75, 100] }
 end
